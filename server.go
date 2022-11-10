314e0f5d704364e41c57b49cbda30936e52c2fc0
RIC21000 <melericcardo9400@gmail.com>
2022-11-10 22:05:59 +0100

    instable commit, contains bugs

--- server.go
+++ server.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"strings"
 )
 
+type socket struct {
+	socket   net.Conn
+	pseudo   string
+	password string
+}
+
+var sockets []socket
+var loginInfos []string
+
 func main() {
+	loginInfos = append(loginInfos, "sltXD:123mdr")
+	loginInfos = append(loginInfos, "test:jspmdr")
 	log.Println("Starting server...")
 	server, err := net.Listen("tcp", ":8888")
 	if err != nil {
@@ -16,7 +29,40 @@ func main() {
 		if err != nil {
 			log.Print(err)
 		}
-		go listenMsg(conn.RemoteAddr().String(), conn)
+		go processClient(conn)
+	}
+}
+
+func processClient(conn net.Conn) {
+	var slice []byte
+	n, err := conn.Read(slice)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	infos := string(slice[:n])
+	var contains bool
+	for _, info := range loginInfos {
+		if infos == info {
+			contains = true
+		}
+	}
+	if !contains {
+		conn.Write([]byte("no"))
+		conn.Close()
+		return
+	}
+	pseudo := strings.Split(infos, ":")[0]
+	password := strings.Split(infos, ":")[1]
+	sockets = append(sockets, socket{socket: conn, pseudo: pseudo, password: password})
+	broadcast(pseudo + " vient de se connecter au chat !")
+	listenMsg(pseudo, conn)
+}
+
+func broadcast(msg string) {
+	log.Println(msg)
+	for _, socket := range sockets {
+		socket.socket.Write([]byte(msg))
 	}
 }
 
@@ -24,8 +70,24 @@ func listenMsg(sender string, conn net.Conn) {
 	message := make([]byte, 1024)
 	n, err := conn.Read(message)
 	if err != nil {
+		removeElement(conn)
 		log.Print(err)
+		return
 	}
 	messageString := string(message[:n])
-	log.Printf("New message from %s : %s\n", sender, messageString)
+
+	broadcast(fmt.Sprintf("%s : %s\n", sender, messageString))
+}
+
+func removeElement(element net.Conn) {
+	var index int = -1
+	for i, socket := range sockets {
+		if socket.socket == element {
+			index = i
+		}
+	}
+	if index == -1 {
+		return
+	}
+	sockets = append(sockets[:index], sockets[index+1:]...)
 }
